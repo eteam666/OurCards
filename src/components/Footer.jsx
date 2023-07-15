551cce395f0888d75d135e3ebd097e3c27d26103
eteam666 <122675543+eteam666@users.noreply.github.com>
2023-07-15 09:00:07 +0800

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+import { Container } from '@/components/Container'
+import { Logo } from '@/components/Logo'
+import { NavLink } from '@/components/NavLink'
+
+export function Footer() {
+  return (
+    <footer className="bg-slate-50">
+      <Container>
+        <div className="py-16">
+        </div>
+        <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
+          <p className="mt-6 text-sm text-slate-500 sm:mt-0">
+            Copyright &copy; {new Date().getFullYear()} BYS. All rights
+            reserved.
+          </p>
+        </div>
+      </Container>
+    </footer>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Build Your Site</title>
+        <meta
+          name="description"
+          content="构建你的网站"
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
